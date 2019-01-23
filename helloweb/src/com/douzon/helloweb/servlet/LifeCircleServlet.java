e04c14322fb71e5ed1267d0ec55d8cfc7ce39884
choi <choi@192.168.0.70>
2019-01-23 20:04:34 +0900

    up

--- helloweb/src/com/douzon/helloweb/servlet/LifeCircleServlet.java
+++ helloweb/src/com/douzon/helloweb/servlet/LifeCircleServlet.java
@@ -1,6 +1,9 @@
 package com.douzon.helloweb.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,32 +13,38 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LifeCircleServlet
  */
-@WebServlet("/LifeCircleServlet")
+@WebServlet("/lc")
 public class LifeCircleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LifeCircleServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public LifeCircleServlet() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init() called");
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("service() called");
+		super.service(request, response);
+	}
+
+	public void destroy() {
+		System.out.println("destroy() called");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet() called");
+		
+		PrintWriter out=response.getWriter();
+		out.println("<h3>Servlet's lifecircle test</h3>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("doPost() called");
 	}
 
 }
